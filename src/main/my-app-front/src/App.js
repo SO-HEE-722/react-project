f2ff6d04af33e20c392f10d82e8441c4bc695193
vkvkdl23 <76863081+vkvkdl23@users.noreply.github.com>
2023-03-22 22:24:10 +0900

    sd

--- src/main/my-app-front/src/App.js
+++ src/main/my-app-front/src/App.js
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        이 기기의 IP주소는 {ip}입니다.1233
+        이 기기의 IP주소는 {ip}입니다.1233=
       </header>
     </div>
   );
